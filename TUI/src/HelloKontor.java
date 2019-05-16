400f396c4b1aa172ba93e13d606ea8699c087093
MichaelSauerHTWdS <michael.sauer@htwsaar.de>
2019-05-16 17:35:20 +0200

    Hello Kontor - ein mini Java-Programm

---
+++ TUI/src/HelloKontor.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mike
+ */
+public class HelloKontor {
+    public static void main(String args[]){
+        System.out.println("HelloKontor");
+    }
+    
+}
