a1ed3133a5867638d901cfa92ec917d72e6690d9
khoulaidi <zoubairbair@gmail.com>
2019-06-03 12:50:42 +0200

    ich mach die spaeter

---
+++ TUI/src/Lager.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import ware.Ware;
+
+public class Lager {
+
+	private int id;
+	private List<Ware> waren;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public List<Ware> getWaren() {
+		return waren;
+	}
+	public void setWaren(List<Ware> waren) {
+		this.waren = waren;
+	}
+	
+	public boolean add_Ware(Ware ware) {
+		
+		return true;
+	}
+	public boolean update_Ware(Ware ware) {
+		
+		return true;
+	}
+	public boolean Delete_Ware(Ware ware) {
+		
+		return true;
+	}
+	
+	public void search_Ware(Ware ware) {
+		
+	}
+	public String toString() {
+		return "";
+	}
+	
+}
+
